5557b41bbdf7c69b67950c052c70f1392c90fba6
Naveen <adith.naveen@gmail.com>
2023-06-21 11:08:50 +0530

    written test case for spring boot

--- rest-works/src/test/java/com/ibm/restworks/RestWorksApplicationTests.java
+++ rest-works/src/test/java/com/ibm/restworks/RestWorksApplicationTests.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -18,47 +19,48 @@ import com.ibm.restworks.service.PersonService;
 @SpringBootTest
 class RestWorksApplicationTests {
 
-	
 	@MockBean
-	PersonRepository repository; 
-	
+	PersonRepository repository;
+
 	@Autowired
-	PersonService service; 
-	
-	
-	@Test
-	@DisplayName("To get person by id by mocked repository")
-	public void shouldGetPersonByIdValidCase() {
-		Person p1 = new Person(); 
-		
+	PersonService service;
+
+	Person p1;
+
+	@BeforeEach
+	public void setUp() {
+		p1 = new Person();
+
 		p1.setId("1234");
 		p1.setName("Roshanth");
 		p1.setCity("Bengaluru");
 		p1.setEmail("roshanth@ibm.com");
 		p1.setPhone("9900887766");
-		
-		Mockito.when(repository.findById("1234")).thenReturn(Optional.of(p1)); 
-		
-		
-		// tests 
-		Person retPerson = service.getPersonById("1234"); 
+
+		Mockito.when(repository.findById("1234")).thenReturn(Optional.of(p1));
+		Mockito.when(repository.findById("2233")).thenReturn(Optional.empty());
+
+	}
+
+	@Test
+	@DisplayName("To get person by id by mocked repository")
+	public void shouldGetPersonByIdValidCase() {
+
+		// tests
+		Person retPerson = service.getPersonById("1234");
 		assertEquals("Roshanth", retPerson.getName());
 	}
-	
+
+	@Test
+	public void shouldReturnEmptyForInvalidId() {
+		Person  retPerson = service.getPersonById("2233"); 
+		assertNull(retPerson);
+	}
+	public void shouldInsertThePersonValidCase() {
+
+	}
+
+	public void shouldPostAndGetTwoPersons() {
+
+	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
